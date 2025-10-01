d94c5279d3b34fd106067265fdfcb35a4090194d
akilaisuru <akilaisurukahingala@gmail.com>
2025-10-01 13:02:29 +0530

    delete button imlemented

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -7,6 +7,12 @@ const cleanForm = () => {
   $("#customer-address").val("");
   $("#customer-salary").val("");
 };
+const customer = new Customer("C001", "Shamodha", "Colombo", "1000");
+const cusromer2 = new Customer("C002", "Shamodha", "Colombo", "1000");
+const cusromer3 = new Customer("C003", "Shamodha", "Colombo", "1000");
+customer_array.push(customer);
+customer_array.push(cusromer2);
+customer_array.push(cusromer3);
 
 const loadTable = () => {
   $("#customer-table-body").empty();
@@ -24,8 +30,19 @@ const loadTable = () => {
     $("#customer-table-body").append(dataElement);
   });
 };
-$(".btn btn-warning btn-edit").on("click", (e) => {
-  console.log("btn clicked");
+$(document).on("click", ".btn-edit", function () {
+  const index = $(this).data("index");
+  const customer = customer_array[index];
+  $("#exampleModalLabel").text("Edit Customer");
+  const myModal = new bootstrap.Modal(document.getElementById("exampleModal"));
+  myModal.show();
+
+  $("#customer-id").val(customer.id);
+  $("#customer-name").val(customer.name);
+  $("#customer-address").val(customer.address);
+  $("#customer-salary").val(customer.salary);
+  $("#edit-index").val(index);
+  $("#btn-customer-save").text("update");
 });
 
 $("#btn-customer-save").on("click", (e) => {
@@ -44,13 +61,34 @@ $("#btn-customer-save").on("click", (e) => {
     customer_address,
     customer_salary
   );
-
-  customer_array.push(cusromer);
+  const editIndex = $("#edit-index").val();
+  if (editIndex === "") {
+    customer_array.push(cusromer);
+  } else {
+    customer_array[editIndex] = cusromer;
+  }
 
   loadTable();
-  // $("#exampleModal").modal("hide");
-  // loadTable();
-  cleanForm();
+  const moadlE1 = document.getElementById("exampleModal");
+  const modal = bootstrap.Modal.getInstance(moadlE1);
+  modal.hide();
+
+  // cleanForm();
 });
 
 loadTable();
+$(document).on("click", ".btn-delete", function () {
+  alert("Customer deleted");
+  const index = $(this).data("index");
+  customer_array.splice(index, 1);
+  loadTable();
+});
+
+$("#btn-add-customer").on("click", () => {
+  cleanForm();
+  $("#customer-modal-title").text("Add Customer");
+  $(".btn-customer-save").text("Save");
+
+  const modal = new bootstrap.Modal(document.getElementById("exampleModal"));
+  modal.show();
+});
